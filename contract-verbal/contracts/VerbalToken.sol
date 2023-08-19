3bf794abce0ace3992df814d691834a06fb16c6b
Otaiki1 <otaikisadiq2804@gmail.com>
2023-08-19 20:57:11 +0100

    refac: fix for deployments

--- contract-verbal/contracts/VerbalToken.sol
+++ contract-verbal/contracts/VerbalToken.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
@@ -7,11 +6,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract VerbalToken is ERC20 {
     address public Holder;
 
-    constructor(
-        uint256 totalSupply,
-        address _holder
-    ) ERC20("VerbalToken", "VTC") {
-        Holder = _holder;
-        _mint(_holder, totalSupply);
+    constructor(address holder) ERC20("VerbalToken", "VTK") {
+        Holder = holder;
+        _mint(holder, 100000 * 10 ** decimals());
     }
 }
